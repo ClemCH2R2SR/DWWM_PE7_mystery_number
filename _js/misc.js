6fd2f5a4c4ec02e5d3f8d28921bd5612f9a582d9
Clement <clement.charrassier@gmail.com>
2023-08-02 14:04:59 +0200

    exo mystery number

---
+++ _js/misc.js
@@ -0,0 +1,17 @@
+"use strict;"
+
+
+        function sayMessage(msg)
+        {
+            console.log(msg);
+        }
+
+        function sayHello()
+        {
+            sayMessage("Debut du programme devine le nombre !");
+        }
+
+        function sayGoodbye()
+        {
+            sayMessage("Fin du programme devine le nombre !");
+        }
